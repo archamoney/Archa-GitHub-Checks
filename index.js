46bf7fd624ce02f8d0685065ea875630ec72ad3a
Morgan Pretty <morgan.pretty@archa.com.au>
2020-02-20 13:37:57 +1100

    Attempt to fix the undefined owner error.

--- index.js
+++ index.js
@@ -30,26 +30,25 @@ module.exports = robot => {
   // dependent PRs to have the base that the merged PR was merged into
   robot.on('pull_request.closed', async context => {
     const {github, payload} = context
-    const self = payload.pull_request
+    const closedPullRequest = payload.pull_request
 
-    if (self.base.repo.default_branch === self.head.ref) {
+    if (closedPullRequest.base.repo.default_branch === closedPullRequest.head.ref) {
       return // Skip if the the head is the default branch
     }
 
-    const owner = payload.repository.owner.login
     const repo = payload.repository.name
-    const head = self.head.ref
-    const base = self.base.ref
+    const head = closedPullRequest.head.ref
+    const base = closedPullRequest.base.ref
     const state = 'open'
     const per_page = 100
 
     // Get all open pull requests with a base matching this head
     github.paginate(
-      github.pullRequests.getAll({owner, repo, base: head, state, per_page}),
+      github.pullRequests.getAll({repo, base: head, state, per_page}),
       async page => {
         for (const {number} of page.data) {
           // Change the base to match where the original PR was merged.
-          github.pullRequests.update({owner, repo, number, base})
+          github.pullRequests.update({repo, number, base})
         }
       }
     )
